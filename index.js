5dfe460b71db9f2065aec8320180cf936df70a8c
devjosecarlosteles <devjosecarlosteles@gmail.com>
2022-08-30 22:41:46 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+console.log("===OPERADORES ARITMÉTICOS===");
+
+console.log("soma: " + (1 + 1));
+console.log("subtração: " + (2 - 1));
+console.log("multiplicação: " + (2 * 2));
+console.log("divisão: " + (2 / 2));
+console.log("potência: " + (2 ** 2)); // 4
+
+console.log("incremento: (++)");
+let n = 1;
+console.log(n)
+n++
+console.log(n)
+
+console.log("decremento: (--)");
+let n2 = 2;
+console.log(n2);
+n2--
+console.log(n2);
+
+console.log(3%2)
+
+console.log("===OPERADORES ATRIBUIÇÃO===");
+
+n = 1; // recebe
+console.log("recebe: " + n)
+n += 1;
+console.log("recebe + 1: " + n)
+
+console.log("===OPERADORES COMPARAÇÃO===");
+console.log(2 > 1); // maior que
+console.log(1 < 2); // menor que
+console.log("olá" == "olá"); // igual a
+console.log(1 == "1") 
+console.log(1 === "1") //igual a (verificando o tipo)
+console.log(1 != "1") // diferente 
+console.log(1 !== "1") // diferente (verificando o tipo)
+
+console.log("===OPERADORES LÓGICOS===");
+console.log(true && false); // valor 1 = true E valor 2 = true -> true
+console.log(false || true); // valor 1 true OU valor 2 = true -> true
+
+var s;
+console.log(!s) //not 
